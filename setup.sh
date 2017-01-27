2fa21674b99f862434c6dca0e1d8cea14d6ac837
Eliran Levi <eliran@stratoscale.com>
2017-01-27 19:48:25 +0200

    Add sudo when creating dir in /usr/share

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo Installing PIP requirements...
 sudo pip install -r pip-requirements.txt
 echo Copying scripts...
-mkdir -p /usr/share/workspaces
+sudo mkdir -p /usr/share/workspaces
 sudo cp workspaces.py workspaces.sh /usr/share/workspaces/
 sudo cp workspaces.completion.sh /etc/bash_completion.d/
 echo "To activate, open a new bash instance (executable name is 'workspaces')."
