7f72b9e1c4d8a5461e260de6d6a8287dd334f0f1
Eliran Levi <vandelayind@gmail.com>
2017-03-01 18:47:24 +0200

    Don't crash if workspace does not have a main repo when listing

--- workspaces.py
+++ workspaces.py
@@ -251,9 +251,10 @@ def main():
             print workspace
             workspace_path = os.path.join(workspaces_root_dir, workspace)
             main_repo = get_main_repo(workspace_path)
-            main_repo_path = os.path.join(workspace_path, main_repo)
-            if os.path.exists(main_repo_path):
-                print os.path.join(workspace, main_repo)
+            if main_repo is not None:
+                main_repo_path = os.path.join(workspace_path, main_repo)
+                if os.path.exists(main_repo_path):
+                    print os.path.join(workspace, main_repo)
     elif args.command == 'describe':
         workspaces = get_workspaces(workspaces_root_dir)
         descriptions = dict()
