e047a8d05cd1362eb7acc556f3810c2845251208
Eliran Levi <eliran@stratoscale.com>
2017-09-06 20:47:43 +0300

    Print help when dir does not exist

--- workspaces.sh
+++ workspaces.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+
+function workspaces_help {
+    echo This tool lists workspaces in a predefined directory, and describes the current git HEAD
+    echo in the 'main' git repository, for each workspace.
+    echo
+    echo - To list workspaces, run with no args.
+    echo
+    echo - To go into workspace dir, run with the dir as the first argument.
+    echo
+    echo - To change the workspaces root dir, either:
+    echo "    * set WORKSPACES_ROOT_DIR env var."
+    echo "    * set root_dir in the /etc/workspaces.yml configuration file."
+    echo
+    echo - To set a specific main repository for a workspace, either:
+    echo "    * add a '.workspaces.yml' inside the workspace dir, and add the line 'main_repo: <your_repo_dirname>' to it."
+    echo "    * Add a workspace_to_main_repo dictionary in /etc/workspaces.yml that maps a workspace name to the name of its main repository name"
+}
+
 if [[ -z $1 ]]; then
     inner_workspaces_script describe-all-workspaces
 else
     if [ "$1" == "--help" ]; then
-        echo This tool lists workspaces in a predefined directory, and describes the current git HEAD
-        echo in the 'main' git repository, for each workspace.
-        echo
-        echo - To list workspaces, run with no args.
-        echo
-        echo - To go into workspace dir, run with the dir as the first argument.
-        echo
-        echo - To change the workspaces root dir, either:
-        echo "    * set WORKSPACES_ROOT_DIR env var."
-        echo "    * set root_dir in the /etc/workspaces.yml configuration file."
-        echo
-        echo - To set a specific main repository for a workspace, either:
-        echo "    * add a '.workspaces.yml' inside the workspace dir, and add the line 'main_repo: <your_repo_dirname>' to it."
-        echo "    * Add a workspace_to_main_repo dictionary in /etc/workspaces.yml that maps a workspace name to the name of its main repository name"
+        workspaces_help
     else
         root_dir="`inner_workspaces_script getrootdir`"
         workspace_dir="$root_dir/$1"
@@ -25,6 +30,9 @@ else
             inner_workspaces_script describe-one-workspace $@
         else
             inner_workspaces_script user-command $@
+            if [ $? -ne 0 ]; then
+                workspaces_help
+            fi
         fi
     fi
 fi
