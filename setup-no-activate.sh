5fd18b86b6786c7950a25cae386b6e7bbb8aaf2a
Eliran Levi <eliran@stratoscale.com>
2017-09-21 00:33:31 +0300

    Split setup script to actual setup and executing a new bash instance

---
+++ setup-no-activate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo Installing PIP requirements...
+sudo pip install -r pip-requirements.txt
+echo Copying scripts...
+sudo mkdir -p /usr/share/workspaces
+sudo cp -fr workspaces/* workspaces.sh /usr/share/workspaces/
+sudo cp workspaces.completion.sh /etc/bash_completion.d/
+if [ ! -e "/etc/workspaces.yml" ]; then
+    sudo cp workspaces.yml /etc/
+fi
+echo "To activate, open a new bash instance (executable name is 'workspaces')."
+
