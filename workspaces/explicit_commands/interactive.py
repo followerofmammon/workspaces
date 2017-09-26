900a4dd12f3596e77dba8dc4366d94b2710d56d9
Michael Filanov <michael.filanov@stratoscale.com>
2017-09-26 11:13:23 +0300

    Don't include hidden entries in dirtree

--- workspaces/explicit_commands/interactive.py
+++ workspaces/explicit_commands/interactive.py
@@ -9,7 +9,8 @@ _result = None
 
 
 def pick():
-    fstree = dirtree.DirTree.factory_from_filesystem(configuration.root_dir, max_depth=2, dirs_only=True)
+    fstree = dirtree.DirTree.factory_from_filesystem(configuration.root_dir, max_depth=2, dirs_only=True,
+                                                     include_hidden=False)
     picker = treepicker.TreePicker(fstree, min_nr_options=1, max_nr_options=1)
     global _result
     _result = picker.pick_one()
@@ -20,3 +21,9 @@ def interactive():
     if _result is not None:
         with open("/tmp/_workspaces.chosen", "w") as output_file:
             output_file.write(_result.full_filesystem_path())
+
+
+if __name__ == "__main__":
+    fstree = dirtree.DirTree.factory_from_filesystem("/home", max_depth=2, dirs_only=True, include_hidden=False)
+    import pdb
+    pdb.set_trace()
