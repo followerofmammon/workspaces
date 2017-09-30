e638d7b000538d1788052f356b1cea90120b5893
Eliran Levi <eliran@stratoscale.com>
2017-09-30 12:16:27 +0300

    Fix setup scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-./setup-no-activate.sh
-exec bash
+echo Installing PIP requirements...
+sudo pip install -r requirements.txt -U
+echo Copying scripts...
+sudo mkdir -p /usr/share/workspaces
+sudo cp -fr workspaces/* workspaces.sh /usr/share/workspaces/
+sudo cp workspaces.completion.sh /etc/bash_completion.d/
+if [ ! -e "/etc/workspaces.yml" ]; then
+    sudo cp workspaces.yml /etc/
+fi
+echo "To activate, open a new bash instance (executable name is 'workspaces')."
